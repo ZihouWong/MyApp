0373ee151445d3a7c45c72454b437a214d608069
ZihouWong <wongzihou1997@gmail.com>
2020-07-22 14:08:55 +0800

    Lecture 1

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -9,10 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    var clickCount = 0
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach (0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("😈")
+            }
+            else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
